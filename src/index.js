e4aac3d3eeea25c5f499ed83d4364ebe576cbcd1
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-07-15 12:16:12 +0200

    Ajout routage application

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './index.css';
-import App from './pages/Home/App';
+import Home from './pages/Home';
+import Logements from './pages/Logements';
+import About from './pages/About';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Logements" element={<Logements />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+      <Home />
+    </Router>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
