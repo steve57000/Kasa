38e2c2b236a7ff3cd83ab843768801d3c9bdeba8
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-07-21 13:33:13 +0200

    ajout des imports et création de la data pour la page logement

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,4 +1,43 @@
-import '../../assets/css/App.css';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Collapse from '../../components/Collapse';
+import Gallery from '../../components/Gallery';
+import Rating from '../../components/Rating';
+import Tags from '../../components/Tags';
+
+const Housing = () => {
+  const { id } = useParams();
+  const [houseDetails, setHouseDetails] = useState([]);
+  const [error, setError] = useState(null);
+  const [hostData, setHostData] = useState(null);
+
+  useEffect(() => {
+    const fetchIdData = () => {
+      fetch('../../data.json', {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      })
+        .then(function (response) {
+          // console.log(response);
+          return response.json();
+        })
+        .then(function (myJson) {
+          // console.log(myJson);
+          const houseDetails = myJson.find((object) => object.id === id);
+          setHouseDetails(houseDetails);
+          setHostData(houseDetails.host);
+        });
+    };
+    fetchIdData();
+  }, [id]);
+
+  if (error) {
+    return (
+      <span>Oups, il y a eu un problème ${JSON.stringify(error.message)}.</span>
+    );
+  }
 
 function Logements() {
   return (
@@ -8,6 +47,6 @@ function Logements() {
       </header>
     </div>
   );
-}
+};
 
-export default Logements;
+export default Housing;
