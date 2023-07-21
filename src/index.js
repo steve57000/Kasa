ac4ed697c03128152acc7a2ca0d43c32cd835ee3
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-07-21 13:45:06 +0200

    changement du router en composant

--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Header from './layout/Header';
-
-import Home from './pages/Home';
-import Housing from './pages/Housing';
-import About from './pages/About';
-import ErrorPage from './pages/ErrorPage';
-
+import RouterApp from './Router';
 import './assets/css/index.css';
 
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/housing" element={<Housing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-);
+root.render(<RouterApp />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
