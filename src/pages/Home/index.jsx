0d7b328c2edf5ed3430e0b4342098256a3be0636
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-07-21 13:28:47 +0200

    ajout de la fonction fetch, création de la liste des logements

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,57 @@
 import '../../assets/css/App.css';
 import Banner from '../../assets/img/banner.jpg';
+import { useEffect, useState } from 'react';
+import Card from '../../components/Card';
+
 function Home() {
+  // State (état, données)
+  const [error, setError] = useState(false);
+  const [dataHousing, setDataHousing] = useState([]);
+
+  // Comportement
+  useEffect(() => {
+    function getData() {
+      fetch('data.json', {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      })
+        .then(function (response) {
+          // console.log(response);
+          return response.json();
+        })
+        .then(function (myJson) {
+          // console.log(myJson);
+          setDataHousing(myJson);
+        });
+    }
+    getData();
+  }, []);
+  if (error) {
+    setError(error);
+    return <span>Oups il y a eu un problème ${error.message}</span>;
+  }
+  // affichage (render)
   return (
     <div className="Home">
-      <div className="Home-banner">
-        <img src={Banner} alt="Banner" />
-        <h1>Chez vous, partout et ailleurs</h1>
+      <div className="Banner">
+        <img src={Banner} alt="Banner" className="Banner-img" />
+        <div className="Banner-content">
+          <h1>Chez vous, partout et ailleurs</h1>
+        </div>
+      </div>
+
+      <div className="SectionCard">
+        <ul>
+          {dataHousing.map((card, index) => (
+            <Card
+              title={card.title}
+              key={`${card.title}-${index}`}
+              id={card.id}
+            ></Card>
+          ))}
+        </ul>
       </div>
     </div>
   );
