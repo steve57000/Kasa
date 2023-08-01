91ece27658746988feae667cf498891b3c5800d5
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-08-01 11:42:11 +0200

    migration de fetch data

---
+++ src/utils/hooks/index.jsx
@@ -0,0 +1,41 @@
+
+const urlHousingsAll: string =  "/data.json"
+
+const getAll = async () => {
+    let response
+    let data: Array
+
+    try {
+        response = await fetch(urlHousingsAll)
+        data = await response.json()
+
+    } catch (err) {
+        throw new Error('Erreur lors de la requete API :' + err)
+    }
+
+    return data
+};
+
+
+const getById = async (id) => {
+    let response
+    let allData
+    let data
+
+    try {
+        response = await fetch(urlHousingsAll)
+        allData = await response.json()
+        data = allData.filter(housing => housing.id === id)[0]
+
+    } catch (err) {
+        throw new Error('Erreur lors de la requete API :' + err)
+    }
+
+    return data
+};
+
+const HousingService = {
+    getAll,
+    getById
+};
+export default HousingService;
