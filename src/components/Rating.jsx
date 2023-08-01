430610098b1df936bf34dcd401d98be88d580e7c
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-08-01 11:03:23 +0200

    création du composant étoile

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,13 +6,20 @@ const Rating = ({ rating }) => {
   const stars = [];
   for (let i = 0; i < 5; i++) {
     if (i < rating) {
-      stars.push(<img src={fullStar} alt="full-star" key={i} />);
+      stars.push(
+        <img
+          src={fullStar}
+          alt="full-star"
+          style={{ width: '32', height: '32' }}
+          key={i}
+        />,
+      );
     } else {
       stars.push(<img src={emptyStar} alt="empty-star" key={i} />);
     }
   }
 
-  return <div className="rating">{stars}</div>;
+  return <div className="Rating">{stars}</div>;
 };
 
 export default Rating;
