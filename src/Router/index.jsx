a66ae5cd6c4d3f955d47cfc2a46a0f7b6955c26a
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-08-01 11:41:44 +0200

    ajout de la route de la page d'erreur

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../layout/Header';
+import Footer from '../layout/Footer';
 import Home from '../pages/Home';
 import Housing from '../pages/Housing';
 import About from '../pages/About';
 import ErrorPage from '../pages/ErrorPage';
-
+import { Error404Redirect } from '../utils/notFoundUrl';
 function RouterApp() {
   return (
     <React.StrictMode>
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/housing/:id" element={<Housing />} />
           <Route path="/about" element={<About />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="*" element={<Error404Redirect />} />
+          <Route path="/404notFound" element={<ErrorPage />} />
         </Routes>
+        <Footer />
       </Router>
     </React.StrictMode>
   );
