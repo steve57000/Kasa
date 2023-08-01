f0e2f071e78b52e03c70d3f96b3407a1e27f7f45
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-08-01 11:04:52 +0200

    création du footer

---
+++ src/layout/Footer/index.jsx
@@ -0,0 +1,22 @@
+import { ReactComponent as Logo } from '../../logoWhite.svg';
+
+function Footer() {
+  return (
+    <div
+      className="Footer"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div className="FooterContent">
+        {/* Logo is an actual React component */}
+        <Logo className="FooterLogo" />
+        <p>© 2020 Kasa. All rights reserved</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
