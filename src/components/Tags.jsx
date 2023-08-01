54251571cf41229d62b93dd287b202319f04ff83
steve57000 <73243226+steve57000@users.noreply.github.com>
2023-08-01 11:04:17 +0200

    ajout de styled-components

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+import colors from '../utils/style/colors';
+const TagContainer = styled.div`
+  font-size: 1vmin;
+  min-width: 320px;
+  display: flex;
+  gap: 10px;
+`;
 
+const TagContent = styled.div`
+  min-width: 84px;
+  max-width: 115px;
+  min-height: 18px;
+  max-height: 25px;
+  width: 100%;
+  height: 100%;
+  background-color: ${colors.primary};
+  color: ${colors.secondary};
+  border-radius: 5px;
+  //margin-right: 10px;
+  display: flex;
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+`;
 const Tags = ({ tags }) => {
   return (
-    <div className="tags">
+    <TagContainer>
       {tags.map((tag) => {
         return (
-          <div className="tag" key={tag}>
+          <TagContent className="Tags" key={tag}>
             {tag}{' '}
-          </div>
+          </TagContent>
         );
       })}
-    </div>
+    </TagContainer>
   );
 };
 
