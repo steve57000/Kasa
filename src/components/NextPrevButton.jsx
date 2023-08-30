7aaa0e1849f836ed5db33d99d9b295c8769958d3
Bell Steve <steve57b@hotmail.fr>
2023-08-30 15:43:32 +0200

    test

--- src/components/NextPrevButton.jsx
+++ src/components/NextPrevButton.jsx
@@ -1,6 +1,10 @@
 import leftArrow from '../assets/img/arrow_left.svg';
 import React from 'react';
+import { string } from 'prop-types';
 
 export default function NextPrevButton({ altSource }) {
   return <img src={leftArrow} alt={altSource} className={altSource} />;
 }
+NextPrevButton.propTypes = {
+  altSource: string,
+};
