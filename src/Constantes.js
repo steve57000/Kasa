55dbcf6dabe6ede5cce99ee1f111b4c502c0f111
Bell Steve <steve57b@hotmail.fr>
2023-09-01 13:25:37 +0200

    ajout homepage

--- src/Constantes.js
+++ src/Constantes.js
@@ -1,8 +1,8 @@
 const production = {
-  url: 'https://steve57000.github.io/kasa',
+  url: 'https://steve57000.github.io/home',
 };
 const development = {
-  url: 'http://localhost:3000',
+  url: 'http://localhost:3000/home',
 };
 export const config =
   process.env.NODE_ENV === 'development' ? development : production;
