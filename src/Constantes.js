827f6adbae828d2c1bb78b6baca95ea0dfdeecb4
Bell Steve <steve57b@hotmail.fr>
2023-09-07 14:07:23 +0200

    test URL git

--- src/Constantes.js
+++
@@ -1,8 +0,0 @@
-const production = {
-  url: 'https://steve57000.github.io',
-};
-const development = {
-  url: 'http://localhost:3000',
-};
-export const config =
-  process.env.NODE_ENV === 'development' ? development : production;
