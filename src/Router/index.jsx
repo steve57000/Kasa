e364591ef5a947bded57572304799a8508a156f8
Bell Steve <steve57b@hotmail.fr>
2023-09-14 17:06:54 +0200

    rename path error page

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -13,7 +13,9 @@ import Housing from '../pages/Housing';
 import About from '../pages/About';
 import ErrorPage from '../pages/ErrorPage';
 import { Error404Redirect } from '../utils/notFoundUrl';
+
 import { config } from "../Constantes"
+
 const urlBase: string = config.url
 function BasicLayout() {
   return (
@@ -27,9 +29,9 @@ function BasicLayout() {
 function RouterApp() {
   return (
     <React.StrictMode>
-      <Router>
-        <Routes basename={`${urlBase}`}>
-          <Route path="" element={<BasicLayout />}>
+      <Router base={`${urlBase}/`}>
+        <Routes>
+          <Route path="/" element={<BasicLayout />}>
             <Route index element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/housing/:id" element={<Housing />} />
