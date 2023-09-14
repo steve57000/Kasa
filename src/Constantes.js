78c8721c8c1b97ffc0a3ac4dce4ac9bef08c5792
Bell Steve <steve57b@hotmail.fr>
2023-09-14 16:54:50 +0200

    rename path error page

--- src/Constantes.js
+++ src/Constantes.js
@@ -1,8 +1,8 @@
 const production = {
-  url: 'https://steve57000.github.io',
+  url: 'https://steve57000.github.io/kasa',
 };
 const development = {
-  url: 'http://localhost:3000/',
+  url: 'http://localhost:3000',
 };
 export const config =
   process.env.NODE_ENV === 'development' ? development : production;
